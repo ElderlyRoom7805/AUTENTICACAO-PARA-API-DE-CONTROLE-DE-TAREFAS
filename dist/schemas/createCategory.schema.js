a96b20eebef62fb164368c34cb73360ee55c81d8
ElderlyRoom7805 <jubiscleudogaymipray@gmail.com>
2025-02-16 19:41:18 -0300

    .

--- dist/schemas/createCategory.schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.categoryBodySchema = exports.createCategorySchema = void 0;
-const zod_1 = require("zod");
-exports.createCategorySchema = zod_1.z.object({
-    id: zod_1.z.number().min(1),
-    name: zod_1.z.string().min(1)
-});
-exports.categoryBodySchema = exports.createCategorySchema.omit({ id: true });
--- dist/schemas/createTask.schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTaskSchema = void 0;
-const zod_1 = require("zod");
-exports.createTaskSchema = zod_1.z.object({
-    title: zod_1.z.string().min(1),
-    content: zod_1.z.string().min(1),
-    categoryId: zod_1.z.number().optional()
-});
